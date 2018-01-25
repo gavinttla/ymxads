ae9015e205e8b290884c8d95d19f8043551400a6
Gavin Cui <gbcui@torchmarkcorp.com>
2018-01-24 19:32:36 -0800

    put in admin assets

---
+++ resources/lang/cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须大于等于6位，确认密码也一致。',
+    'reset' => '你的密码已经从新发送！',
+    'sent' => '我们已经把重置密码链接发到你的邮箱了！',
+    'token' => '此密码重置标识为无效。',
+    'user' => "我们找不到此邮箱关联的用户。",
+
+];
