32a4c425759fab21255e2656f143471e7f8fdb47
Gavin Cui <gavintt@gmail.com>
2018-03-06 03:43:42 +0000

    add upload directory, change header and footer content

--- config/admin.php
+++ config/admin.php
@@ -5,17 +5,17 @@ return [
     /*
      * Laravel-admin name.
      */
-    'name' => 'Laravel-admin',
+    'name' => 'valiquan-admin',
 
     /*
      * Logo in admin panel header.
      */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>Valiquan</b> admin',
 
     /*
      * Mini-logo in admin panel header.
      */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>VL</b>',
 
     /*
      * Route configuration.
